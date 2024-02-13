24df1ecb51cc40f47fc40961b2fff73e728a7bfd
SamaDawihi <90503910+SamaDawihi@users.noreply.github.com>
2024-02-13 21:05:42 +0300

    Task 3.1 Get

--- app/Http/Controllers/Api/V1/PropertyController.php
+++ app/Http/Controllers/Api/V1/PropertyController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Controllers\Controller;
 use App\Models\Property;
 use App\Http\Requests\StorePropertyRequest;
 use App\Http\Requests\UpdatePropertyRequest;
@@ -13,7 +14,7 @@ class PropertyController extends Controller
      */
     public function index()
     {
-        //
+        return Property::all();
     }
 
     /**
