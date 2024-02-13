7529ed562d69902ac3c69fa5b8de572a74ad8154
SamaDawihi <90503910+SamaDawihi@users.noreply.github.com>
2024-02-13 21:10:38 +0300

    3.2 Setting Up Seeder For Testing

--- database/factories/PropertyFactory.php
+++ database/factories/PropertyFactory.php
@@ -16,8 +16,22 @@ class PropertyFactory extends Factory
      */
     public function definition(): array
     {
+        //id, title, address, price, bedrooms, bathrooms, type (like apartment, villa, etc.), and status (like available, sold, etc.)
+
+        //setting random elements based on type: villa or apartment
+        $type = $this->faker->randomElement(['A', 'V']);
+        $price = $type == 'A'? $this->faker->numberBetween(10000, 100000) : $this->faker->numberBetween(100000, 10000000);
+        $bedrooms = $type == 'A'? $this->faker->numberBetween(1, 5) : $this->faker->numberBetween(3, 10);
+        $bathrooms = $this->faker->numberBetween($bedrooms, $bedrooms + 3);
+        
         return [
-            //
-        ];
+            'title' => $this->faker->sentence(),
+            'address' => $this->faker->address(),
+            'price' => $price,
+            'bedrooms' => $bedrooms,
+            'bathrooms' => $bathrooms,
+            'type' => $type,
+            'status' => $this->faker->randomElement(['A', 'S'])
+        ];;
     }
 }
--- database/seeders/PropertySeeder.php
+++ database/seeders/PropertySeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Property;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +12,6 @@ class PropertySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Property::factory(100)->create();
     }
 }
